bf5bea5d239e7a9b211cf6d6f75871618d63ad1f
Cory House <housecor@gmail.com>
2017-02-27 06:36:24 -0600

    Initial config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactAtellier from 'react-atellier';
+import TextInput from './components/TextInput';
+
+const componentList = [{
+  componentName : "dognuts",
+  component : TextInput
+}];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactAtellier components={componentList} />
     );
   }
 }
