f5dab26373c9018a0e725852b157636b2a409e01
Cory House <housecor@gmail.com>
2017-03-10 07:08:11 -0600

    Refactor - eliminate async and organize

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -17,10 +17,8 @@ function writeFile(filename, content) {
   });
 }
 
-function readFile(filePath, callback) {
-  fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-    err ? console.log(err) : callback(data);
-  });
+function readFile(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
 }
 
 function getExampleFiles(componentName) {
@@ -33,53 +31,41 @@ function getExampleFiles(componentName) {
   return exampleFiles;
 }
 
-function generate(componentsPath, examplesPath, callback) {
-  const componentData = [];
-  const directories = getDirectories(componentsPath);
-  const numDirectories = directories.length;
-  let numDirectoriesProcessed = 0;
-
-  directories.map(componentName => {
-    readFile(path.join(componentsPath, componentName, componentName + '.js'), content => {
-      let info = parse(content);
-      let component = {
-        name: componentName,
-        description: info.description,
-        props: info.props,
-        code: content,
-        examples: []
-      };
-
-      let examples = getExampleFiles(componentName);
-      let numExamples = examples.length;
-      if (numExamples === 0) numDirectoriesProcessed++;
-      let numExamplesProcessed = 0;
-      examples.map(file => {
-        let examplePath = path.join(examplesPath, componentName, file)
-        readFile(examplePath, content => {
-          let info = parse(content);
-          component.examples.push({
-            name: file.slice(0, -3), //Remove the .js extension,
-            path: path.join('examples', componentName), // TODO: Make this configurable or move to Examples.js.
-            description: info.description,
-            code: content
-          });
-          numExamplesProcessed++;
-          let componentProcessingComplete = numExamplesProcessed == numExamples;
-          if (componentProcessingComplete) {
-            numDirectoriesProcessed++;
-            componentData.push(component);
-            let jobComplete = numDirectoriesProcessed == numDirectories;
-            if (jobComplete) callback(componentData);
-          }
-        });
-      });
-    });
+function getExampleData(examplesPath, componentName) {
+  let examples = getExampleFiles(componentName);
+  return examples.map(file => {
+    let filePath = path.join(examplesPath, componentName, file)
+    let content = readFile(filePath)
+    let info = parse(content);
+    return {
+      name: file.slice(0, -3), //Remove the .js extension,
+      path: path.join('examples', componentName), // TODO: Make this configurable or move to Examples.js.
+      description: info.description,
+      code: content
+    };
   });
+}
+
+function getComponentData(componentPath, examplePath, componentName) {
+  const content = readFile(path.join(componentPath, componentName, componentName + '.js'));
+  let info = parse(content);
+  return {
+    name: componentName,
+    description: info.description,
+    props: info.props,
+    code: content,
+    examples: getExampleData(examplePath, componentName)
+  }
+}
+
+function generate(componentsPath, examplesPath) {
+  const componentData = getDirectories(componentsPath).map(componentName => {
+    return getComponentData(componentsPath, examplesPath, componentName);
+  });
+
+  writeFile('componentData.js', "export default " + JSON.stringify(componentData));
 }
 
 const examplesPath = path.join(__dirname, '../src', 'docs', 'examples');
 const componentsPath = path.join(__dirname, '../src', 'components');
-generate(componentsPath, examplesPath, componentData => {
-    writeFile('componentData.js', "export default " + JSON.stringify(componentData));
-});
+generate(componentsPath, examplesPath);
