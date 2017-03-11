523cc2ebb3d6e66cc9a9370f54fe85d32ce9fae3
Cory House <housecor@gmail.com>
2017-03-11 10:24:05 -0600

    Add test

--- src/components/RegistrationFormContainer/RegistrationFormContainer.spec.js
+++ src/components/RegistrationFormContainer/RegistrationFormContainer.spec.js
@@ -6,8 +6,6 @@ import RegistrationFormContainer from './RegistrationFormContainer';
 it('should show 2 error message when submit button is clicked on empty form', () => {
   const wrapper = mount(<RegistrationFormContainer onSubmit={() => {}} />);
   wrapper.find({type: 'submit'}).simulate('click');
-
-  // 2 error messages should display after clicking submit on empty form
   let errors = wrapper.find('.error');
   expect(errors).toHaveLength(2);
 });
