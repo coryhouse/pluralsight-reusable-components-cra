76c0c8c8b166765b67b9634c1a9dc440294ca48a
Cory House <housecor@gmail.com>
2017-03-11 08:35:53 -0600

    Enhance tests

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,28 +1,69 @@
 import React from 'react';
-import PasswordInput from './PasswordInput';
-import {shallow} from 'enzyme';
-
-// Note that this import can be commented out once
+import renderer from 'react-test-renderer';
+// Note that this import can be commented out if
 // we declare the jestSetup in package.json because
 // Enzyme functions are globally available for tests.
-// import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
+import PasswordInput from './PasswordInput';
 
-describe('PasswordInput', () => {
-  // Behavioral test
-  it('Toggles input type when show/hide password clicked', () => {
-    const wrapper = shallow(<PasswordInput
-     name="test"
-     onChange={() => {}}
-     showVisibilityToggle={true}
-     maxLength={50}/>
-    );
+// Behavioral test
+it('Toggles input type when show/hide password clicked', () => {
+  const wrapper = shallow(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    showVisibilityToggle
+    />
+  );
 
-    // Password input should have a type of password initially.
-    expect(wrapper.find({type: 'password'})).toHaveLength(1);
-    wrapper.find('a').simulate('click');
+  // Password input should have a type of password initially.
+  expect(wrapper.find({type: 'password'})).toHaveLength(1);
+  wrapper.find('a').simulate('click');
 
-    // Password input should have a type of text after clicking toggle.
-    let passwordInput = wrapper.find({type: 'text'});
-    expect(passwordInput).toHaveLength(1);
-  });
+  // Password input should have a type of text after clicking toggle.
+  let passwordInput = wrapper.find({type: 'text'});
+  expect(passwordInput).toHaveLength(1);
+});
+
+test('should display tips with bad password', () => {
+  const tree = renderer.create(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    showTips={true}
+    value="a" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should show no tips with good password', () => {
+  const tree = renderer.create(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    showTips={true}
+    value="Uisi38#8iad" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should hide password quality by default', () => {
+  const tree = renderer.create(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    value="Uisi38#8iad" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should show password quality when enabled and a password is entered', () => {
+  const tree = renderer.create(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    showQuality
+    value="Uisi38#8iad" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should hide password quality when enabled but no password is entered', () => {
+  const tree = renderer.create(<PasswordInput
+    name="test"
+    onChange={() => {}}
+    showQuality
+    value="" />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -2,20 +2,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import ProgressBar from './ProgressBar';
 
-// Snapshot test
-describe('ProgressBar', () => {
-  test('should render red with 20%', () => {
-    const component = renderer.create(<ProgressBar percent={20} />).toJSON();
-    expect(component).toMatchSnapshot();
-  });
+test('should render red with 20%', () => {
+  const tree = renderer.create(<ProgressBar percent={20} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  test('should render light green with 60%', () => {
-    const component = renderer.create(<ProgressBar percent={60} />).toJSON();
-    expect(component).toMatchSnapshot();
-  });
+test('should render light green with 60%', () => {
+  const tree = renderer.create(<ProgressBar percent={60} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  test('should render dark green with 100%', () => {
-    const component = renderer.create(<ProgressBar percent={100} />).toJSON();
-    expect(component).toMatchSnapshot();
-  });
+test('should render dark green with 100%', () => {
+  const tree = renderer.create(<ProgressBar percent={100} />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
