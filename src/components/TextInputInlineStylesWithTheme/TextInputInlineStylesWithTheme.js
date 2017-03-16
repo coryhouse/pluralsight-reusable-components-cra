c1a11edcea4fa820d115cc1bce0fd9fb8bd6f453
Cory House <housecor@gmail.com>
2017-03-16 18:11:26 -0500

    Move to external stylesheet with centralized variables

--- src/components/TextInputInlineStylesWithTheme/TextInputInlineStylesWithTheme.js
+++ src/components/TextInputInlineStylesWithTheme/TextInputInlineStylesWithTheme.js
@@ -1,22 +1,28 @@
 import React, {PropTypes} from 'react';
+import defaultTheme from './defaultTheme';
 
 /**
- * Themeable textInput styled using React's inline styles. Some styles are overridden based on the theme prop. Note how some of the component's built-in styles are always enforced. For example, the input always has a red border when an error occurs, even though the theme allows overriding the non-error state. Note that you could centralize styling variables in a js file if used in multiple components.
+ * Themeable textInput styled using React's inline styles.
+ * Some styles are overridden based on the theme prop.
+ * Note how some of the component's built-in styles are always enforced.
+ * For example, the input always has a red border when an error occurs,
+ * even though the theme allows overriding the non-error state.
+ * Note that you could centralize styling variables in a js
+ * file if used in multiple components.
  */
 const TextInput = ({name, label, type = "text", required = false, onChange, placeholder, value, error, children, theme={fontColor: 'black', accentColor: 'darkgray', borderWidth: '1px'}, ...props}) => {
   // Allow overriding the default input border color via theme.
   // But always set border color to the defaultTheme's error border (red) when there is an error.
-  const errorColor = 'red';
-  const inputBorderColor = (error && error.length > 0) ? errorColor : theme.accentColor;
+  const inputBorderColor = (error && error.length > 0) ? defaultTheme.error.color : theme.accentColor;
   let inputStyle = {
     color: theme.fontColor,
     border: `solid ${theme.borderWidth} ${inputBorderColor}`
   };
 
   return (
-    <div style={{marginBottom: 16}}>
+    <div style={{marginBottom: defaultTheme.fieldset.marginBottom}}>
       <label htmlFor={name}>{label}</label>
-      { required && <span style={{color: errorColor}}> *</span> }
+      { required && <span style={{color: defaultTheme.error.color}}> *</span> }
       <div className="field">
         <input
           type={type}
@@ -27,7 +33,7 @@ const TextInput = ({name, label, type = "text", required = false, onChange, plac
           style={inputStyle}
           {...props}/>
           {children}
-        {error && <div style={{color: errorColor}}>{error}</div>}
+        {error && <div style={{color: defaultTheme.error.color}}>{error}</div>}
       </div>
     </div>
   );
