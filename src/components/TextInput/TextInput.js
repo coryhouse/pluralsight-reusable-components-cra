4dc71cae6745b08f4fd4592f0304adea78bb9d49
Cory House <housecor@gmail.com>
2017-03-25 09:42:14 -0500

    Fix styling, add htmlId, fix htmlFor

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 
-const TextInput = ({name, label, type = "text", required = false, onChange, placeholder, value, error, children, ...props}) => {
+const TextInput = ({htmlId, label, type = "text", required = false, onChange, placeholder, value, error, children, ...props}) => {
   const inputStyle = {display: 'inline-block'};
   if (error && error.length > 0) {
     inputStyle.border = 'solid 1px red';
@@ -8,11 +8,12 @@ const TextInput = ({name, label, type = "text", required = false, onChange, plac
 
   return (
     <div style={{marginBottom: 16}}>
-      <label htmlFor={name}>{label}</label>
-      { required && <span style={{color: 'red'}}> *</span> }
+      <label style={{display: 'block'}} htmlFor={htmlId}>
+        {label} { required && <span style={{color: 'red'}}> *</span> }
+      </label>
       <input
         type={type}
-        name={name}
+        id={htmlId}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
@@ -25,7 +26,7 @@ const TextInput = ({name, label, type = "text", required = false, onChange, plac
 };
 
 TextInput.propTypes = {
-  name: PropTypes.string.isRequired,
+  htmlId: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.oneOf(['text', 'number', 'date', 'password']),
   required: PropTypes.bool,
