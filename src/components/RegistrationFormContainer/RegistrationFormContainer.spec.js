1a8230db5fd357b5c7c7d06ccd4fd28c4e41f1b3
Cory House <housecor@gmail.com>
2017-03-25 09:58:21 -0500

    Add comment

--- src/components/RegistrationFormContainer/RegistrationFormContainer.spec.js
+++ src/components/RegistrationFormContainer/RegistrationFormContainer.spec.js
@@ -4,6 +4,7 @@ import {mount} from 'enzyme';
 import RegistrationFormContainer from './RegistrationFormContainer';
 
 it('should show 2 error messages when submit button is clicked on empty form', () => {
+  // Note that I'm using mount so that all child components are rendered as well
   const wrapper = mount(<RegistrationFormContainer onSubmit={() => {}} />);
   wrapper.find({type: 'submit'}).simulate('click');
   let errors = wrapper.find('.textinput__input--state-error ');
