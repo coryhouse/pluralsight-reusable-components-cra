0318955023b59bc22bc3f81f9b5598c2ec476a87
Cory House <housecor@gmail.com>
2017-03-26 15:27:46 -0500

    Rename

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,66 +1,134 @@
-import React, { PropTypes } from 'react';
-import PasswordInput from '../PasswordInput';
+import React, {PropTypes} from 'react';
 import TextInput from '../TextInput';
+import PasswordInput from '../PasswordInput';
 
 /**
- * Registration form that requires email and password
+ * Registration form with built-in validation.
  */
-const RegistrationForm = ({email, password, errors, onChange, onSubmit, submitButtonValue="Register"}) => {
-  return (
-    <div>
-      <TextInput
-        htmlId="registration-form-email"
-        onChange={onChange}
-        label="Email"
-        value={email}
-        error={errors.email}
-        required />
+class RegistrationForm extends React.Component {
+  static defaultProps = {
+    confirmationMessage: "Thanks for registering!"
+  };
 
-      <PasswordInput
-        htmlId="registration-form-password"
-        value={password}
-        onChange={onChange}
-        showQuality
-        showTips
-        showVisibilityToggle
-        maxLength={50}
-        error={errors.password} />
+  constructor(props) {
+    super(props);
 
-      <input type="submit" value={submitButtonValue} onClick={onSubmit} />
-    </div>
-  );
+    this.state = {
+      user: {
+        email: '',
+        password: ''
+      },
+      errors: {},
+      submitted: false,
+    };
+
+    // Need not be in state since not used in render
+    this.passwordMinLength = 8
+  }
+
+  onChange = (event) => {
+    const user = this.state.user;
+    user[event.target.name] = event.target.value;
+    this.setState({user});
+  }
+
+  // Returns a number from 0 to 100 that represents password quality.
+  passwordQuality(password) {
+    if (!password) return null;
+    let score = 0;
+    const {hasAlpha, hasNumber, hasSpecialChar, meetsMinLength} = this.getPasswordAttributes(password);
+    if (hasAlpha) score += 10;
+    if (hasNumber) score += 10;
+    if (hasSpecialChar) score += 10;
+
+    if (meetsMinLength) {
+      score += 70;
+    } else {
+      // Password hasn't reached minimum length, so cap score to 60, regardless of length.
+      // This way the user doesn't see a full 100% quality green bar until min length is reached.
+      score += (password.length * 10 > 60) ? 60 : password.length * 10;
+    }
+    return score;
+  }
+
+  getPasswordAttributes(password) {
+    return {
+      hasAlpha: password.match(/[a-z]/g),
+      hasNumber: password.match(/\d+/g),
+      hasSpecialChar: password.match(/[^a-zA-Z0-9]+/g),
+      meetsMinLength: password.length >= this.passwordMinLength
+    };
+  }
+
+  validate({email, password}) {
+    const errors = {};
+
+    if (!email) {
+      errors.email = 'Email required.';
+    }
+
+    // For now, just enforcing a minimum length.
+    if (password.length < this.passwordMinLength) {
+      errors.password = `Password must be at least ${this.passwordMinLength} characters.`;
+    }
+
+    this.setState({errors});
+    const formIsValid = Object.getOwnPropertyNames(errors).length === 0;
+    return formIsValid;
+  }
+
+  onSubmit = () => {
+    const {user} = this.state;
+    const formIsValid = this.validate(user);
+    if (formIsValid) {
+      this.props.onSubmit(user);
+      this.setState({submitted: true});
+    }
+  }
+
+  render() {
+    const {errors, submitted} = this.state;
+    const {email, password} = this.state.user;
+
+    return (
+      submitted ?
+      <h2>{this.props.confirmationMessage}</h2> :
+      <div>
+        <TextInput
+          htmlId="registration-form-email"
+          name="email"
+          onChange={this.onChange}
+          label="Email"
+          value={email}
+          error={errors.email}
+          required />
+
+        <PasswordInput
+          htmlId="registration-form-password"
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          quality={this.passwordQuality(password)}
+          showVisibilityToggle
+          maxLength={50}
+          error={errors.password} />
+
+        <input type="submit" value="Register" onClick={this.onSubmit} />
+      </div>
+    )
+  }
 }
 
 RegistrationForm.propTypes = {
   /**
-   * User email address
+   * Message displayed upon successful submission
    */
-  email: PropTypes.string.isRequired,
+  confirmationMessage: PropTypes.string,
 
   /**
-   * User password
+   * Called when form is submitted
    */
-  password: PropTypes.string.isRequired,
-
-  /**
-   * Validation errors - Each error should have its own key
-   */
-  errors: PropTypes.object.isRequired,
-
-  /**
-   * Function to call on submit
-   */
-  onSubmit: PropTypes.func.isRequired,
-
-  /**
-   * Function to call on input change
-   */
-  onChange: PropTypes.func.isRequired,
-
-  /**
-   * Submit button label
-   */
-  submitButtonValue: PropTypes.string
-};
+  onSubmit: PropTypes.func.isRequired
+}
 
 export default RegistrationForm;
--- src/components/RegistrationForm/RegistrationForm.spec.js
+++ src/components/RegistrationForm/RegistrationForm.spec.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {mount} from 'enzyme';
-import RegistrationFormContainer from './RegistrationFormContainer';
+import RegistrationForm from './RegistrationForm';
 
 it('should show 2 error messages when submit button is clicked on empty form', () => {
   // Note that I'm using mount so that all child components are rendered as well
-  const wrapper = mount(<RegistrationFormContainer onSubmit={() => {}} />);
+  const wrapper = mount(<RegistrationForm onSubmit={() => {}} />);
   wrapper.find({type: 'submit'}).simulate('click');
   let errors = wrapper.find('.error');
   expect(errors).toHaveLength(2);
