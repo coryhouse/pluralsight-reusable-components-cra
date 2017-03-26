d70099e44d2540447f23ff480fdbb0bfda6ec273
Cory House <housecor@gmail.com>
2017-03-26 07:51:17 -0500

    Eliminate dumb component

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,40 +1,85 @@
 import React, { PropTypes } from 'react';
-import PasswordInput from '../PasswordInput';
 import TextInput from '../TextInput';
+import PasswordInput from '../PasswordInput';
 
-const RegistrationForm = ({email, password, errors, onChange, onSubmit, submitButtonValue="Register"}) => {
-  return (
-    <div>
-      <TextInput
-        htmlId="registration-form-email"
-        onChange={onChange}
-        label="Email"
-        value={email}
-        error={errors.email}
-        required />
+/**
+ * Easily create a registration form with built-in validation. Just pass a function to call when the user's information is submitted.
+ */
+class RegistrationFormContainer extends React.Component {
+  static defaultProps = {
+    confirmationMessage: "Thanks for registering!"
+  };
 
-      <PasswordInput
-        htmlId="registration-form-password"
-        value={password}
-        onChange={onChange}
-        showQuality
-        showTips
-        showVisibilityToggle
-        maxLength={50}
-        error={errors.password} />
+  constructor(props) {
+    super(props);
 
-      <input type="submit" value={submitButtonValue} onClick={onSubmit} />
-    </div>
-  );
+    this.state = {
+      user: {
+        email: '',
+        password: ''
+      },
+      errors: {},
+      submitted: false
+    };
+  }
+
+  onChange = (event) => {
+    const user = this.state.user;
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  }
+
+  onSubmit = () => {
+    const errors = {};
+    const { user } = this.state;
+    if (!user.email) errors.email = 'Email required.';
+    if (!user.password) errors.password = 'Password required.';
+    this.setState({ errors });
+    const validationPassed = Object.getOwnPropertyNames(errors).length === 0;
+
+    if (validationPassed) {
+      this.props.onSubmit(user);
+      this.setState({ submitted: true });
+    }
+  }
+
+  render() {
+    const { errors, submitted } = this.state;
+    const { email, password } = this.state.user;
+
+    return (
+      submitted ?
+        <h2>{this.props.confirmationMessage}</h2> :
+        <div>
+          <TextInput
+            htmlId="registration-form-email"
+            name="email"
+            onChange={onChange}
+            label="Email"
+            value={email}
+            error={errors.email}
+            required />
+
+          <PasswordInput
+            htmlId="registration-form-password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            showQuality
+            showTips
+            showVisibilityToggle
+            maxLength={50}
+            error={errors.password} />
+
+          <input type="submit" value={submitButtonValue} onClick={onSubmit} />
+        </div>
+    )
+  }
 }
 
-RegistrationForm.propTypes = {
-  email: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-  errors: PropTypes.object.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  submitButtonValue: PropTypes.string
-};
+RegistrationFormContainer.propTypes = {
+  confirmationMessage: PropTypes.string,
+  onSubmit: PropTypes.func.isRequired
+}
 
-export default RegistrationForm;
+export default RegistrationFormContainer;
