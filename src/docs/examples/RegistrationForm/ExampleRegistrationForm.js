e3c4163fb36a1929dbeb4d832b333d9162ea0dd8
Cory House <housecor@gmail.com>
2017-03-26 15:29:48 -0500

    Rename

--- src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
+++ src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
@@ -1,55 +1,14 @@
 import React from 'react';
 import RegistrationForm from 'ps-ui/RegistrationForm';
 
-/**
- * Example registration form
- */
-export default class ExampleRegistrationForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: {
-        email: '',
-        password: ''
-      },
-      errors: {},
-      submitted: false
-    };
-  }
-
-  onChange = (event) => {
-    const user = this.state.user;
-    user[event.target.name] = event.target.value;
-    this.setState({user});
-  }
-
+class ExampleRegistrationForm extends React.Component {
   onSubmit = () => {
-    const errors = {};
-    const {user} = this.state;
-    if (!user.email) errors.email = 'Email required.';
-    if (!user.password) errors.password = 'Password required.';
-    this.setState({errors});
-    const validationPassed = Object.getOwnPropertyNames(errors).length === 0;
-
-    if (validationPassed) {
-      // Submit user on success here
-      this.setState({submitted: true});
-    }
+    // Place logic here
   }
 
   render() {
-    const {errors, submitted} = this.state;
-    const {email, password} = this.state.user;
-    return (
-      submitted ?
-      <h2>Thanks for registering!</h2> :
-      <RegistrationForm
-        email={email}
-        password={password}
-        errors={errors}
-        onChange={this.onChange}
-        onSubmit={this.onSubmit} />
-    )
+    return <RegistrationForm onSubmit={this.onSubmit} />
   }
 }
+
+export default ExampleRegistrationForm;
