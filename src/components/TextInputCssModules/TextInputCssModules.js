cf2092d2daa4cd91c163aeba5d657931924bee16
Cory House <housecor@gmail.com>
2017-03-31 16:13:37 -0500

    Remove comments since unused

--- src/components/TextInputCssModules/TextInputCssModules.js
+++ src/components/TextInputCssModules/TextInputCssModules.js
@@ -2,9 +2,6 @@ import React, {PropTypes} from 'react';
 import Label from 'ps-ui/Label';
 import styles from './textInput.css';
 
-/**
- * Abstraction over text input to enforce validation, label, and required field marker consistency
- */
 const TextInputCssModules = ({htmlId, name, label, type = "text", required = false, onChange, placeholder, value, error, children, ...props}) => {
   let inputClass = '';
   if (error && error.length > 0) {
@@ -31,49 +28,14 @@ const TextInputCssModules = ({htmlId, name, label, type = "text", required = fal
 };
 
 TextInputCssModules.propTypes = {
-  /**
-   * Unique HTML ID. Used for tying label to HTML input. Handy hook for automated testing.
-   */
   htmlId: PropTypes.string.isRequired,
-
-  /**
-   * Input name
-   */
   name: PropTypes.string.isRequired,
-
-  /**
-   * Input label
-   */
   label: PropTypes.string.isRequired,
-
-  /**
-   * Input type
-   */
   type: PropTypes.oneOf(['text', 'number', 'date', 'password']),
-
-  /**
-   * Mark label with asterisk if set to true
-   */
   required: PropTypes.bool,
-
-  /**
-   * Function to call onChange
-   */
   onChange: PropTypes.func.isRequired,
-
-  /**
-   * Placeholder to display when empty
-   */
   placeholder: PropTypes.string,
-
-  /**
-   * Value
-   */
   value: PropTypes.string,
-
-  /**
-   * String to display when error occurs
-   */
   error: PropTypes.string
 };
 
