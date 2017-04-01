7893b93784c0928bb6c1dfae1319b53f0a5a3494
Cory House <housecor@gmail.com>
2017-04-01 15:48:51 -0500

    Add textinputbem

---
+++ src/components/TextInputBem/TextInputBem.js
@@ -0,0 +1,40 @@
+import React, {PropTypes} from 'react';
+import Label from '../Label';
+//import './textInput.css';
+
+/**
+ * Abstraction over text input to enforce consistency in validation, labels, and required field marker.
+ * Using BEM to encapsulate styling.
+ */
+const TextInputBem = ({htmlId, name, label, type = "text", required = false, onChange, placeholder, value, error, children, ...props}) => {
+  return (
+    <div className="textinput">
+      <Label htmlFor={htmlId} label={label} required={required} />
+      <input
+        id={htmlId}
+        type={type}
+        name={name}
+        className={error && 'textinput__input--state-error'}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        {...props}/>
+        {children}
+      {error && <div className="textinput__error">{error}</div>}
+    </div>
+  );
+};
+
+TextInputBem.propTypes = {
+  htmlId: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['text', 'number', 'password']),
+  required: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+export default TextInputBem;
