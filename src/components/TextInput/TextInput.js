dcf48e763dc92bdd90c57da5a55d21eb96e059e3
Cory House <housecor@gmail.com>
2017-03-31 22:13:08 -0500

    Remove unnecessary style

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -5,7 +5,7 @@ import Label from 'ps-ui/Label';
  * Abstraction over text input with integrated label to enforce consistency in layout, error display, label placement, and required field marker.
  */
 const TextInput = ({htmlId, name, label, type = "text", required = false, onChange, placeholder, value, error, children, ...props}) => {
-  const inputStyle = {display: 'inline-block'};
+  const inputStyle = {};
   if (error && error.length > 0) {
     inputStyle.border = 'solid 1px red';
   }
