aa8aa9475b506dab73b2e2478d52223a13b6aca2
Cory House <housecor@gmail.com>
2017-04-16 20:39:20 -0500

    Add HelloWorld

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'ps-ui/HelloWorld';
+
+/** With a custom message: */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
