bfc6ec95d83c578410e9dd479a87f80c754d1b73
Cory House <housecor@gmail.com>
2017-04-23 09:57:03 -0500

    Enhance example

--- src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
+++ src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import RegistrationForm from 'ps-ui/RegistrationForm';
+import RegistrationForm from 'ps-react/RegistrationForm';
 
 export default class ExampleRegistrationForm extends React.Component {
-  onSubmit = () => {
-    // Place logic here
+  onSubmit = (user) => {
+    console.log(user);
   }
 
   render() {
